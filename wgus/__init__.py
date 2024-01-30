f72bfdc75fcc1893fef3a1cb005adc864b1184d0
Renat Iliev <me@izeberg.me>
2024-01-31 00:03:40 +0400

    v0.0.10

--- wgus/__init__.py
+++ wgus/__init__.py
@@ -1,6 +1,6 @@
 """API for Wargaming Update Service (WGUS)"""
 
-__version__ = "0.0.7"
+__version__ = "0.0.10"
 
 from .metadata import Metadata, get_metadata
 from .patches_chains import PatchesChains, get_patches_chains
